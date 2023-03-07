f14e58ab76ac9a4ea44a3f4876b42fa8b09121d3
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-07 15:04:59 +0530

    implemented root file app.js. commented routes

---
+++ app.js
@@ -0,0 +1,31 @@
+const db=require('./models');
+const express=require("express");
+const app=express();
+
+//const apiRoutes=require("./Routes/apiRoutes");
+//app.use(apiRoutes);
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+
+
+const PORT= process.env.PORT||8000; 
+
+db.sequelize.sync()
+.then((res)=>{
+    app.listen(PORT,()=>{
+        console.log(`listening to PORT=${PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("error creating the database schema in the file");
+    console.log(err);
+});
+
+module.exports=app; //FOR TESTING
+
+
+
+
