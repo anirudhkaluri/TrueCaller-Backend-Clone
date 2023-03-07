b91c6613cd6896d2203252022891901ec6f7b5a0
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-07 19:33:55 +0530

    isInContacts implemented

--- Dao/contactsDao.js
+++ Dao/contactsDao.js
@@ -1,6 +1,10 @@
+const PersonalContact=require('../models');
 
-const isInContacts=(user_id,phone_number)=>{
-
+const isInContacts=(userid,phone_number)=>{
+    const user=PersonalContact.findOne({where:{user_id:userid,phone:phone_Number}});
+    if(user)
+        return true;
+    return false;
 }
 
 module.exports={
