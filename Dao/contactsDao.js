c2de5850b9c3d5c2cc2fcef25bd8a6eab27066c4
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-07 21:28:05 +0530

    when entered phone number it retrieves all the records from personal contacts table with that phone number

--- Dao/contactsDao.js
+++ Dao/contactsDao.js
@@ -7,6 +7,15 @@ const isInContacts=(userid,phone_number)=>{
     return false;
 }
 
+const getAllEntries=async (phone_number)=>{
+    const users=await PersonalContact.findAll({
+        where:{phone:phone_number},
+        attributes:['user_id','name','phone','-email']
+    });
+    return users;
+}
+
 module.exports={
-    isInContacts
+    isInContacts,
+    getAllEntries
 }
