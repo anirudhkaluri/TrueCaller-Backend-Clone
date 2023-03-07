862b68b84217f4833f54daaacabde7518e885895
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-07 18:50:44 +0530

    getUser made reusable by sending both user id and status

--- Dao/userDao.js
+++ Dao/userDao.js
@@ -1,20 +1,22 @@
 const {User,Credential}=require('../models');
 
 const {Sequelize}=require('sequelize');
-const sequelize=new Sequelize('global_database','root','root',{dialect:'sqlite',storage:'../global_database.sqlite'});
+const sequelize=new Sequelize('global_database','root','root',{
+    dialect:'sqlite',
+    storage:'../global_database.sqlite',
+});
 
 const userExists=async (phoneNumber)=>{
     try{    
         const user=await User.findOne({where:{phone:phoneNumber}}); 
         if(user)
-            return true;     
+            return {status:true,user_id:user.user_id};     
     }
     catch(error){
         console.log('error while checking if phone number already exists');
         throw error;
     }
-    
-    return false;
+    return {status:false};
 }
 
 
@@ -22,23 +24,8 @@ const userExists=async (phoneNumber)=>{
 const add_new_user=async (new_user_details)=>{
    
 
-    try{
-        // await sequelize.transaction(async(t)=>{
-
-        //     const user_model_instance=await User.create({
-        //         name:new_user_details.name,
-        //         phone:new_user_details.phone,
-        //         email:new_user_details.email
-        //     },{transaction:t});
-
-        //     console.log("NEW MODEL INSTANCE CREATED IS:",user_model_instance);
-
-        //     const credential_model_instance=await Credential.create({
-        //         user_id:user_model_instance.user_id,
-        //         password:new_user_details.password
-        //     },{transaction:t});
-        // });
-
+        //TO DO CONVERT IT INTO A TRANSACTION
+     
         const user_model_instance=await User.create({
             name:new_user_details.name,
             phone:new_user_details.phone,
@@ -50,11 +37,6 @@ const add_new_user=async (new_user_details)=>{
             password:new_user_details.password
         });
         
-    }
-    catch(error){
-        console.log('error creating new user',error);
-        throw error;
-    }
 }
 
 module.exports={
