de14578ac1aa3ecace61260ee69b101ef520daac
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-07 14:55:30 +0530

    model for Personal Contacts created

---
+++ models/PersonalContact.js
@@ -0,0 +1,34 @@
+module.exports=(sequelize,DataTypes)=>{
+    const PersonalContact=sequelize.define("PersonalContact",{
+        user_id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            references:{
+                model:"Users",
+                key:"user_id"
+            }
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                notEmpty:true
+            }
+        },
+        phone:{
+            type:DataTypes.STRING(10),
+            allowNull:false,
+            unique:true,
+            validate:{
+                notEmpty:true,
+                isNumeric:true,
+                len:[10,10]
+            }
+        },
+        email:{
+            type:DataTypes.STRING,
+            allowNull:true,
+        }
+    });
+    return PersonalContact;
+}
