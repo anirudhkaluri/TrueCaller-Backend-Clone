2cb7e5965ec20a5c09ed32781f78ce286bb9b448
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-07 22:08:21 +0530

    search with number implemented

--- Services/searchService.js
+++ Services/searchService.js
@@ -1,6 +1,7 @@
 
-const {userExists}=require('../Dao/userDao');
+const {userExists,getRegisteredUsersCount}=require('../Dao/userDao');
 const {getAllEntries}=require('../Dao/contactsDao');
+const {getSpamHits}=require('../Dao/spamDao');
 
 const search_with_name=(req,res)=>{
     const search_term=req.params.name;
@@ -8,18 +9,40 @@ const search_with_name=(req,res)=>{
 }
 
 const search_with_number=async (req,res)=>{
+    //TODO: check if the cookie is set in request object (LOGIN CHECK)
     const search_number=req.params.phone;
-    const users=await userExists(search_number);
-    let response_json;
-    if(users!== null){
-       
+    const user=await userExists(search_number);
+    let users;
+    let response_data;
+    const total_registered_users=await getRegisteredUsersCount();
+    if(user=== null){
+        users=await getAllEntries(search_number);
+        for(let i=0;i<users.length;i++){
+            var spam_data=await getSpamHits(users[i].phone);
+            const spam_liklihood=spam_liklihood_calculator(spam_data.spam_hits,spam_data.spammers_count,total_registered_users);
+            users[i]["spam_liklihood"]=spam_liklihood;
+        }
+        response_data=users;
     }
     else{
-        users=getAllEntries(search_number);
+        delete user.email;
+        const spam_data=await getSpamHits(user.phone);
+        const spam_liklihood=spam_liklihood_calculator(spam_data.spam_hits,spam_data.spammers_count,total_registered_users);
+        response_data={"name":user.name,"phone":user.phone,"email":user.email,"spam_liklihood":spam_liklihood};
     }
+    res.send(response_data);
+}
+
+const spam_liklihood_calculator=(spam_hits,spam_table_length,registered_users_count)=>{
+
+    var weight1=(spam_hits/spam_table_length)*100;
+    var weight2=(spam_hits/registered_users_count)*100;
+    
+    return (weight1+weight2);
 }
 
 module.exports={
     search_with_name,
-    search_with_number
+    search_with_number,
+    spam_liklihood_calculator
 }
