846807af18467973545bfc39e748cc14d7cf6b9d
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-14 20:38:39 +0530

    implemented the jwt authenticate token method

--- Services/authFunctions.js
+++ Services/authFunctions.js
@@ -2,11 +2,36 @@ const jwt=require('jsonwebtoken');
 const dotenv=require('dotenv');
 dotenv.config();
 
-//IMPORT THE TOKEN SECRET
-process.env.TOKEN_SECRET;
 
 function generateAccessToken(userid){
     return jwt.sign(userid, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+function authenticateToken(req, res, next) {
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+     
+  
+      if (err) {
+        console.log(err);
+        return res.sendStatus(403); //send error code
+      }
+  
+      req.user = user
+  
+      next()
+    })
+}
+
+
+module.exports={
+    generateAccessToken,
+    authenticateToken
 }
 
 
